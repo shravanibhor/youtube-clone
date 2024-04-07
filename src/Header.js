4c5215406f27999406722b1f3507cc1c3383b74b
Shravani <shravanibhor3084@gmail.com>
2024-04-07 16:29:42 +0530

    header

--- src/Header.js
+++ src/Header.js
@@ -2,19 +2,34 @@ import React from 'react';
 import MenuIcon from "@mui/icons-material/Menu"; 
 import SearchIcon from "@mui/icons-material/Search";
 import VideocallIcon from "@mui/icons-material/VideoCall";
+import "./Header.css";
 import AppIcon from "@mui/icons-material/Apps";
+import NotificationIcon from "@mui/icons-material/Notifications";
+import Avatar from "@mui/material/Avatar";
 
 function header() {
   return (
-    <div className="Header">
+    <div className="header">
+
+      <div className='heade_left'>
       <MenuIcon/>
       <img 
       className='header_logo'
       src="https://www.freeiconspng.com/uploads/youtube-logo-1.png"></img>
-      <input type='text'></input>
-      <SearchIcon/>
+      </div>
+
+      <div className='header_input'>
+      <input placeholder="Search" type='text'></input>
+      <SearchIcon className='header_inputButton'/>
+      </div>
+
+      <div className='header_right'>
       <VideocallIcon/>
       <AppIcon/>
+      <NotificationIcon/>
+      <Avatar alt="Shravani" src="https://imagetolink.com/ib/JS4k23RFvx"/>
+      </div>
+
     </div>
   )
 }
